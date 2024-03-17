826fae12cc22faa1f4e05ac6a9307420481cc9f3
Darmawan Setiawan <dermawans1442@gmail.com>
2024-03-17 10:57:32 +0800

    create method

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -10,8 +10,13 @@ public class PersonApp {
     System.out.println(person1.address);
     System.out.println(person1.country);
 
+    person1.sayHello("Setiawan");
+
     Person person2 = new Person();
+
     Person person3;
     person3 = new Person();
+    person3.name = "der";
+    person3.sayHello("awan");
   }
 }
