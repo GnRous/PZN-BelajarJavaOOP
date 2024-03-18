bb26b9f53c0effef4d2dd0896251388e8f70e239
Darmawan Setiawan <dermawans1442@gmail.com>
2024-03-18 13:39:00 +0800

    implementation of Method Overriding

--- src/Manager.java
+++ src/Manager.java
@@ -3,6 +3,6 @@ class Manager {
   String name;
 
   void sayHello(String name) {
-    System.out.println("hi " + name + ", My Name is " + this.name);
+    System.out.println("hi " + name + ", My Name is Manager " + this.name);
   }
 }
--- src/VicePresident.java
+++ src/VicePresident.java
@@ -1,3 +1,6 @@
 class VicePresident extends Manager{
 
+  void sayHello(String name) {
+    System.out.println("hi " + name + ", My Name is VP " + this.name);
+  }
 }
