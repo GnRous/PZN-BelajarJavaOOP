36548ff9990cb5c771bf15d6bc25c4eeec92b741
Darmawan Setiawan <dermawans1442@gmail.com>
2024-03-18 13:59:20 +0800

    implementation of "super-Constructor"

--- src/ManagerApp.java
+++ src/ManagerApp.java
@@ -1,12 +1,10 @@
 public class ManagerApp {
   public static void main(String[] args) {
 
-    var manager = new Manager();
-    manager.name = "Derm";
+    var manager = new Manager("Derm");
     manager.sayHello("Setya");
 
-    var vp = new VicePresident();
-    vp.name = "Joko";
+    var vp = new VicePresident("Joko");
     vp.sayHello("Setya");
   }
 }
--- src/VicePresident.java
+++ src/VicePresident.java
@@ -1,5 +1,9 @@
 class VicePresident extends Manager{
 
+  VicePresident(String name){
+    super(name);
+  }
+
   void sayHello(String name) {
     System.out.println("hi " + name + ", My Name is VP " + this.name);
   }
