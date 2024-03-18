f9301226065e4fdb5599ab21165bdcbf4e3de9fa
Darmawan Setiawan <dermawans1442@gmail.com>
2024-03-18 09:06:44 +0800

    added constructor Person

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,10 +1,7 @@
 public class PersonApp {
   public static void main(String[] args) {
 
-    var person1 = new Person();
-    person1.name = "Darmawan";
-    person1.address = "Makassar";
-    //person1.country = "America"; //ERROR
+    var person1 = new Person("Dermawan", "Makassar");
 
     System.out.println(person1.name);
     System.out.println(person1.address);
@@ -12,11 +9,10 @@ public class PersonApp {
 
     person1.sayHello("Setiawan");
 
-    Person person2 = new Person();
+    Person person2 = new Person("Setiawan", "Makassar");
 
     Person person3;
-    person3 = new Person();
-    person3.name = "der";
-    person3.sayHello("awan");
+    person3 = new Person("Joko", "Cirebon");
+    person3.sayHello("Setiawan");
   }
 }
