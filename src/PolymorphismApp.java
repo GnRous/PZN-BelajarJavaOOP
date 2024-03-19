c6fc3c5731af3caafc0b2c7934049ef136d35803
Darmawan Setiawan <dermawans1442@gmail.com>
2024-03-19 09:44:42 +0800

    implementation of Polymorphism

---
+++ src/PolymorphismApp.java
@@ -0,0 +1,22 @@
+public class PolymorphismApp {
+  public static void main(String[] args) {
+
+
+    Employee employee = new Employee("Derm");
+    employee.sayHello("Setya");
+
+    employee = new Manager("Derm");
+    employee.sayHello("Setya");
+
+    employee = new VicePresident("Derm");
+    employee.sayHello("Setya");
+
+    sayHello(new Employee("Derm"));
+    sayHello(new Manager("Setya"));
+    sayHello(new VicePresident("Wan"));
+  }
+
+  static void sayHello(Employee employee) {
+    System.out.println("Hello " + employee.name);
+  }
+}
